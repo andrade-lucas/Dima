47a6a9bfe6b02fb33d38ff73a7a05f8677ce11fb
Lucas Andrade <lucasilva108@gmail.com>
2024-09-12 00:50:34 -0300

    feat: v1/transactions initial

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,43 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost(
+        "/v1/transactions", 
+        (Request request, Handler handler) => handler.Handle(request)
+    )
+    .WithName("Transactions: Create")
+    .WithSummary("Cria uma nova transação")
+    .Produces<Response>();
 
 app.Run();
+
+// Request
+public class Request
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
+// Response
+public class Response
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
+
+// Handler
+public class Handler
+{
+    public Response Handle(Request request)
+    {
+        return new Response
+        {
+            Id = 4,
+            Title = request.Title
+        };
+    }
+}
