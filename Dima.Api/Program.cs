29bc3197744ab8acad308aff9e2a5df33dbca311
Lucas Andrade <lucasilva108@gmail.com>
2024-09-15 12:52:47 -0300

    implement create, update and delete category endpoints

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,4 +1,10 @@
 using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,7 +21,7 @@ builder.Services.AddSwaggerGen(x =>
 {
     x.CustomSchemaIds(n => n.FullName);
 });
-builder.Services.AddTransient<Handler>();
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 
 var app = builder.Build();
 
@@ -23,42 +29,31 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapPost(
-        "/v1/transactions", 
-        (Request request, Handler handler) => handler.Handle(request)
+        "/v1/categories", 
+        (CreateCategoryRequest request, ICategoryHandler handler) => handler.CreateAsync(request)
     )
-    .WithName("Transactions: Create")
-    .WithSummary("Cria uma nova transação")
-    .Produces<Response>();
+    .WithName("Categories: Create")
+    .WithSummary("Cria uma nova categoria")
+    .Produces<Response<Category?>>();
+
+app.MapPut(
+        "/v1/categories/{id}", 
+        (long id, UpdateCategoryRequest request, ICategoryHandler handler) => {
+            request.Id = id;
+            handler.UpdateAsync(request);
+        })
+    .WithName("Categories: Update")
+    .WithSummary("Atualiza uma nova categoria")
+    .Produces<Response<Category?>>();
+
+app.MapDelete(
+        "/v1/categories/{id}", 
+        (long id, [FromBody]DeleteCategoryRequest request, ICategoryHandler handler) => {
+            request.Id = id;
+            handler.DeleteAsync(request);
+        })
+    .WithName("Categories: Delete")
+    .WithSummary("Deleta uma nova categoria")
+    .Produces<Response<Category?>>();
 
 app.Run();
-
-// Request
-public class Request
-{
-    public string Title { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public int Type { get; set; }
-    public decimal Amount { get; set; }
-    public long CategoryId { get; set; }
-    public string UserId { get; set; } = string.Empty;
-}
-
-// Response
-public class Response
-{
-    public long Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-}
-
-// Handler
-public class Handler
-{
-    public Response Handle(Request request)
-    {
-        return new Response
-        {
-            Id = 4,
-            Title = request.Title
-        };
-    }
-}
