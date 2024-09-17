f7dac447c4a1136dcc61142cb513b43090d4b267
Lucas Andrade <lucasilva108@gmail.com>
2024-09-17 00:06:20 -0300

    refactor: remove category's endpoints from program class

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,4 +1,5 @@
 using Dima.Api.Data;
+using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
 using Dima.Core.Handlers;
 using Dima.Core.Models;
@@ -27,63 +28,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet(
-        "/v1/categories", 
-        async (ICategoryHandler handler) => {
-            var request = new GetAllCategoriesRequest
-            {
-                UserId = "test@balta.io"
-            };
-            return await handler.GetAllAsync(request);
-        })
-    .WithName("Categories: Get All")
-    .WithSummary("Obter todas as categorias de um usuário")
-    .Produces<PagedResponse<List<Category>?>>();
-
-app.MapGet(
-        "/v1/categories/{id}", 
-        async (long id, ICategoryHandler handler) => {
-            var request = new GetCategoryByIdRequest
-            {
-                Id = id,
-                UserId = "test@balta.io"
-            };
-            return await handler.GetByIdAsync(request);
-        })
-    .WithName("Categories: Get By Id")
-    .WithSummary("Obter categoria por Id")
-    .Produces<Response<Category?>>();
-
-app.MapPost(
-        "/v1/categories", 
-        async (CreateCategoryRequest request, ICategoryHandler handler) => await handler.CreateAsync(request)
-    )
-    .WithName("Categories: Create")
-    .WithSummary("Cria uma nova categoria")
-    .Produces<Response<Category?>>();
-
-app.MapPut(
-        "/v1/categories/{id}", 
-        async (long id, UpdateCategoryRequest request, ICategoryHandler handler) => {
-            request.Id = id;
-            return await handler.UpdateAsync(request);
-        })
-    .WithName("Categories: Update")
-    .WithSummary("Atualiza uma nova categoria")
-    .Produces<Response<Category?>>();
-
-app.MapDelete(
-        "/v1/categories/{id}", 
-        async (long id, ICategoryHandler handler) => {
-            var request = new DeleteCategoryRequest
-            {
-                Id = id,
-                UserId = "test@balta.io"
-            };
-            return await handler.DeleteAsync(request);
-        })
-    .WithName("Categories: Delete")
-    .WithSummary("Deleta uma nova categoria")
-    .Produces<Response<Category?>>();
+app.MapGet("/", () => new { message = "OK" });
+app.MapEndpoints();
 
 app.Run();
