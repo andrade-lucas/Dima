672fd7aed574a226916f6f5244f130ef3446300d
Lucas Andrade <lucasilva108@gmail.com>
2024-09-17 00:04:52 -0300

    refactor: create category endpoint

--- Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
+++ Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -10,21 +10,19 @@ public class CreateCategoryEndpoint : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app)
     {
-        app.MapPost(
-            "/",
-            HandleAsync 
-        )
-        .WithName("Categories: Create")
-        .WithSummary("Cria uma nova categoria")
-        .Produces<Response<Category?>>();
+        app.MapPost("/", HandleAsync)
+            .WithName("Categories: Create")
+            .WithSummary("Cria uma nova categoria")
+            .WithOrder(2)
+            .Produces<Response<Category?>>();
     }
 
     private static async Task<IResult> HandleAsync(ICategoryHandler handler, CreateCategoryRequest request)
     {
         var result = await handler.CreateAsync(request);
-        if (result.IsSuccess)
-            return Results.Created($"{result.Data?.Id}", result);
-        
-        return Results.BadRequest(result.Data);
+
+        return result.IsSuccess
+            ? TypedResults.Created($"{result.Data?.Id}", result)
+            : TypedResults.BadRequest(result.Data);
     }
 }
