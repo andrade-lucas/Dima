d0af8f84552d487f888fca0b8a802e1d18b25029
Lucas Andrade <lucasilva108@gmail.com>
2024-09-26 21:08:18 -0300

    feat: get transaction by period request

--- Dima.Core/Requests/Transactions/GetTransactionsByPeriodRequest.cs
+++ Dima.Core/Requests/Transactions/GetTransactionsByPeriodRequest.cs
@@ -1,6 +1,6 @@
 namespace Dima.Core.Requests.Transactions;
 
-public class GetTransactionsByPeriod : PagedRequest
+public class GetTransactionsByPeriodRequest : PagedRequest
 {
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
