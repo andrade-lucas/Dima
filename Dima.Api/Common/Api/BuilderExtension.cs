2fcee7641bd204dacf800210774b507010b8658d
Lucas Andrade <lucasilva108@gmail.com>
2024-09-30 21:51:19 -0300

    feat: configure cors

--- Dima.Api/Common/Api/BuilderExtension.cs
+++ Dima.Api/Common/Api/BuilderExtension.cs
@@ -13,6 +13,8 @@ public static class BuilderExtension
     public static void AddConfiguration(this WebApplicationBuilder builder)
     {
         Configuration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+        Configuration.FrontendUrl = builder.Configuration.GetValue<string>("FrontendUrl") ?? string.Empty;
     }
 
     public static void AddDocumentation(this WebApplicationBuilder builder)
@@ -46,6 +48,18 @@ public static class BuilderExtension
 
     public static void AddCrossOrigin(this WebApplicationBuilder builder)
     {
+        builder.Services.AddCors(x =>
+        {
+            x.AddPolicy(ApiConfiguration.CorsPolicyName, policy =>
+                policy.WithOrigins([
+                        Configuration.BackendUrl,
+                        Configuration.FrontendUrl
+                    ])
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+            );
+        });
     }
 
     public static void AddServices(this WebApplicationBuilder builder)
