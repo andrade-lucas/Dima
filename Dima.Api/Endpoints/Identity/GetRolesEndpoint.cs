23a93db3fdd5ce228047662a18d04dc860e3eac1
Lucas Andrade <lucasilva108@gmail.com>
2024-09-30 21:27:13 -0300

    feat: create get roles and logout endpoint files

---
+++ Dima.Api/Endpoints/Identity/GetRolesEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Dima.Api.Common.Api;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Identity;
+
+public class GetRolesEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/roles", HandleAsync)
+            .WithTags("Identity")
+            .RequireAuthorization();
+    }
+
+    private static IResult HandleAsync(ClaimsPrincipal user)
+    {
+        if (user.Identity is null || !user.Identity.IsAuthenticated)
+            return Results.Unauthorized();
+
+        var identity = (ClaimsIdentity)user.Identity;
+        var roles = identity.FindAll(identity.RoleClaimType)
+            .Select(c => new
+            {
+                c.Issuer,
+                c.OriginalIssuer,
+                c.Type,
+                c.Value,
+                c.ValueType
+            });
+
+        return TypedResults.Json(roles);
+    }
+}
