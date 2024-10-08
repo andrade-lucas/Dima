25dee893f9d47d0e0113116c6925f6e0b110a6ca
Lucas Andrade <lucasilva108@gmail.com>
2024-10-07 23:09:07 -0300

    feat: create codebehind to register interface

---
+++ Dima.Web/Pages/Identity/Register.razor.cs
@@ -0,0 +1,23 @@
+﻿using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Identity;
+
+public class RegisterPage : ComponentBase
+{
+    #region Dependencies
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    [Inject]
+    public IAccountHandler Handler { get; set; } = null!;
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+
+    [Inject]
+    public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+    #endregion
+}
