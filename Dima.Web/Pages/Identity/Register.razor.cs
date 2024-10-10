742365d99249cd3e2768b8ca79481ea56d399c18
Lucas Andrade <lucasilva108@gmail.com>
2024-10-09 22:10:59 -0300

    feat: register user code behind

--- Dima.Web/Pages/Identity/Register.razor.cs
+++ Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
@@ -19,5 +20,47 @@ public class RegisterPage : ComponentBase
 
     [Inject]
     public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+    #endregion
+
+    #region Properties
+    public bool IsBusy { get; set; } = false;
+    public RegisterRequest InputModel { get; set; } = new();
+    #endregion
+
+    #region Overrides
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity is { IsAuthenticated: true })
+            NavigationManager.NavigateTo("/");
+    }
+    #endregion
+
+    #region
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+        try
+        {
+            var result = await Handler.RegisterAsync(InputModel);
+
+            if (result.IsSuccess)
+                NavigationManager.NavigateTo("/login");
+            else
+                Snackbar.Add(result.Message, Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Snackbar.Add(ex.Message, Severity.Error);
+            throw;
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
     #endregion
 }
