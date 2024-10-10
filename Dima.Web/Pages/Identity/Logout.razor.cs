42dd727b22c03e5fb71a8900c2385b730e07ed59
Lucas Andrade <lucasilva108@gmail.com>
2024-10-10 00:14:49 -0300

    feat: implement logout

---
+++ Dima.Web/Pages/Identity/Logout.razor.cs
@@ -0,0 +1,37 @@
+﻿using Dima.Core.Handlers;
+using Dima.Web.Security;
+using Microsoft.AspNetCore.Components;
+
+namespace Dima.Web.Pages.Identity;
+
+public partial class LogoutPage : ComponentBase
+{
+    #region Services
+
+    [Inject]
+    public IAccountHandler Handler { get; set; } = null!;
+
+    [Inject]
+    public NavigationManager Navigation { get; set; } = null!;
+
+    [Inject]
+    public ICookieAuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+
+    #endregion
+
+    #region Override
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (await AuthenticationStateProvider.CheckAuthenticatedAsync())
+        {
+            await Handler.LogoutAsync();
+            await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            AuthenticationStateProvider.NotifyAuthenticationStateChanged();
+        }
+
+        await base.OnInitializedAsync();
+    }
+
+    #endregion
+}
