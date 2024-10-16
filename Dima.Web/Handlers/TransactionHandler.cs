53695f2186414c0ccecf8a4c2a5542d97c961f6e
Lucas Andrade <lucasilva108@gmail.com>
2024-10-15 22:13:15 -0300

    feat: create TransactionHandler on web project

---
+++ Dima.Web/Handlers/TransactionHandler.cs
@@ -0,0 +1,60 @@
+﻿using Dima.Core.Common.Extensions;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+using System.Net.Http.Json;
+
+namespace Dima.Web.Handlers;
+
+public class TransactionHandler(IHttpClientFactory httpClientFactory) : ITransactionHandler
+{
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
+    public async Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
+    {
+        var result = await _client.PostAsJsonAsync("v1/transactions", request);
+
+        return await result.Content.ReadFromJsonAsync<Response<Transaction?>>()
+            ?? new Response<Transaction?>(null, 400, "Erro ao criar transação");
+    }
+
+    public async Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+    {
+        var result = await _client.DeleteAsync($"v1/transactions/{request.Id}");
+
+        return await result.Content.ReadFromJsonAsync<Response<Transaction?>>()
+            ?? new Response<Transaction?>(null, 400, "Erro ao excluir transação");
+    }
+
+    public async Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+    {
+        return await _client.GetFromJsonAsync<Response<Transaction?>>($"v1/transactions/{request.Id}")
+            ?? new Response<Transaction?>(null, 400, "Erro ao obter transação por Id");
+    }
+
+    public async Task<PagedResponse<List<Transaction>?>> GetByPeriod(GetTransactionsByPeriodRequest request)
+    {
+        const string format = "yyyy-MM-dd";
+        var startDate = request.StartDate is not null
+            ? request.StartDate.Value.ToString(format)
+            : DateTime.Now.StartOfMonth().ToString(format);
+
+        var endDate = request.EndDate is not null
+            ? request.EndDate.Value.ToString(format)
+            : DateTime.Now.EndOfMonth().ToString(format);
+
+        var url = $"v1/transactions?startDate={startDate}&endDate={endDate}";
+
+        return await _client.GetFromJsonAsync<PagedResponse<List<Transaction>?>>(url)
+            ?? new PagedResponse<List<Transaction>?>(null, 400, "Não foi possível obter as transações");
+    }
+
+    public async Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+    {
+        var result = await _client.PutAsJsonAsync($"v1/transactions/{request.Id}", request);
+
+        return await result.Content.ReadFromJsonAsync<Response<Transaction?>>()
+            ?? new Response<Transaction?>(null, 400, "Erro ao atualizar transação");
+    }
+}
