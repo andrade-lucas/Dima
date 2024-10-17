ad7b54851fd8b75e7f7ee8c67ec99a4ecfcad989
Lucas Andrade <lucasilva108@gmail.com>
2024-10-17 00:39:28 -0300

    feat: edit category

---
+++ Dima.Web/Pages/Categories/Edit.razor.cs
@@ -0,0 +1,104 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Categories;
+
+public partial class EditCategoryPage : ComponentBase
+{
+    #region Properties
+
+    [Parameter]
+    public string Id { get; set; } = string.Empty;
+
+    public bool IsBusy { get; set; } = false;
+    public UpdateCategoryRequest InputModel { get; set; } = new();
+
+    #endregion
+
+    #region Services
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    [Inject]
+    public ICategoryHandler Handler { get; set; } = null!;
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+
+    #endregion
+
+    #region Overrides
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var request = new GetCategoryByIdRequest { Id = long.Parse(Id) };
+            var result = await Handler.GetByIdAsync(request);
+
+            if (result is { IsSuccess: true, Data: not null})
+            {
+                InputModel = new UpdateCategoryRequest
+                {
+                    Id = result.Data.Id,
+                    Title = result.Data.Title,
+                    Description = result.Data.Description,
+                };
+            }
+            else
+            {
+                Snackbar.Add(result.Message, Severity.Error);
+                NavigationManager.NavigateTo("/categorias");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Snackbar.Add(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var result = await Handler.UpdateAsync(InputModel);
+
+            if (result.IsSuccess)
+            {
+                Snackbar.Add(result.Message, Severity.Success);
+                NavigationManager.NavigateTo("/categorias");
+            }
+            else
+                Snackbar.Add(result.Message, Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Snackbar.Add(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    #endregion
+}
