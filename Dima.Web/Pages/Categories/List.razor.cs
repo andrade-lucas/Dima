798e93feefcd868efe6139b604fa647a03af3947
Lucas Andrade <lucasilva108@gmail.com>
2024-10-17 00:39:11 -0300

    feat: delete category

--- Dima.Web/Pages/Categories/List.razor.cs
+++ Dima.Web/Pages/Categories/List.razor.cs
@@ -22,6 +22,9 @@ public partial class ListCategoriesPage : ComponentBase
     public ISnackbar Snackbar { get; set; } = null!;
 
     [Inject]
+    public IDialogService DialogService { get; set; } = null!;
+
+    [Inject]
     public ICategoryHandler Handler { get; set; } = null!;
 
     #endregion
@@ -30,6 +33,7 @@ public partial class ListCategoriesPage : ComponentBase
 
     protected override async Task OnInitializedAsync()
     {
+        Console.WriteLine("Teste hello");
         IsBusy = true;
 
         try
@@ -55,6 +59,42 @@ public partial class ListCategoriesPage : ComponentBase
     #endregion
 
     #region Methods
+
+    public async void OnDeleteButtonClickedAsync(long id, string title)
+    {
+        Console.WriteLine(id.ToString(), title);
+        var result = await DialogService.ShowMessageBox(
+            "ATENÇÃO",
+            $"Ao prosseguir a categoria {title} será excluída. Esta é uma ação irreversível! Deseja continuar?",
+            yesText: "Excluir",
+            cancelText: "Cancelar"
+        );
+
+        if (result is true)
+            await OnDeleteAsync(id, title);
+
+        StateHasChanged();
+    }
+
+    public async Task OnDeleteAsync(long id, string title)
+    {
+        try
+        {
+            var request = new DeleteCategoryRequest
+            {
+                Id = id
+            };
+
+            await Handler.DeleteAsync(request);
+            Categories.RemoveAll(x => x.Id == id);
+            Snackbar.Add($"{title} excluído(a) com sucesso", Severity.Success);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+    }
 
     public Func<Category, bool> Filter => category =>
     {
