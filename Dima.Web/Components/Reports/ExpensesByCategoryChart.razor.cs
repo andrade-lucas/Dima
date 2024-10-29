40c73b162b5ff0db49e5114c9a203515583ea97c
Lucas Andrade <lucasilva108@gmail.com>
2024-10-28 22:05:30 -0300

    feat: create ExpensesByCategory chart

---
+++ Dima.Web/Components/Reports/ExpensesByCategoryChart.razor.cs
@@ -0,0 +1,55 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Reports;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Components.Reports;
+
+public partial class ExpensesByCategoryChartComponent : ComponentBase
+{
+    #region Properties
+
+    public List<double> Data { get; set; } = [];
+
+    public List<string> Labels { get; set; } = [];
+
+    #endregion
+
+    #region Services
+
+    [Inject]
+    public IReportHandler Handler { get; set; } = null!;
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    #endregion
+
+    #region Overrides
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            var result = await Handler.GetExpensesByCategoryReportAsync(new GetExpensesByCategoryRequest());
+
+            if (result is { IsSuccess: true, Data: not null })
+            {
+                result.Data.ForEach(x =>
+                {
+                    Labels.Add(x.Category);
+                    Data.Add((double)x.Expenses * -1);
+                });
+            }
+            else
+                Snackbar.Add(result.Message, Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+    }
+
+    #endregion
+}
