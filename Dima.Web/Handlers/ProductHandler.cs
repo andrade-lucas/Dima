a727ef2cc44c6317fb8001b2215be2c8fd9fd89e
Lucas Andrade <lucasilva108@gmail.com>
2024-11-06 20:59:31 -0300

    feat: add ProductHandler to web project

---
+++ Dima.Web/Handlers/ProductHandler.cs
@@ -0,0 +1,24 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using System.Net.Http.Json;
+
+namespace Dima.Web.Handlers;
+
+public class ProductHandler(IHttpClientFactory httpClientFactory) : IProductHandler
+{
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
+    public async Task<PagedResponse<List<Product>?>> GetAllAsync(GetAllProductsRequest request)
+    {
+        return await _client.GetFromJsonAsync<PagedResponse<List<Product>?>>($"/v1/products?pageNumber={request.PageNumber}&pageSize={request.PageSize}")
+            ?? new PagedResponse<List<Product>?>(null, 400, "Erro ao obter produtos");
+    }
+
+    public async Task<Response<Product?>> GetBySlugAsync(GetProductBySlugRequest request)
+    {
+        return await _client.GetFromJsonAsync<Response<Product?>>($"/v1/products/{request.Slug}")
+            ?? new Response<Product?>(null, 400, "Erro ao obter produto");
+    }
+}
