d652666dea4c40e1da469f05c02d78f52c66054b
Lucas Andrade <lucasilva108@gmail.com>
2024-11-05 23:08:40 -0300

    feat: create voucher endpoint

---
+++ Dima.Api/Endpoints/Orders/GetVoucherByNumberEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class GetVoucherByNumberEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/{number}", HandleAsync)
+            .WithName("Voucher: Get by Number")
+            .WithSummary("Obtém um voucher pelo número")
+            .WithDescription("Obtém um voucher pelo número")
+            .WithOrder(1)
+            .Produces<Response<Voucher?>>();
+
+    private static async Task<IResult> HandleAsync(IVoucherHandler handler, [FromRoute] string number)
+    {
+        var request = new GetVoucherByNumberRequest
+        {
+            Number = number
+        };
+
+        var result = await handler.GetByNumberAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
