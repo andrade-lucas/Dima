4a93c77f38e4f641d1b3d64cb589e1151a9f09ca
Lucas Andrade <lucasilva108@gmail.com>
2024-11-05 23:08:31 -0300

    feat: create product endpoints

---
+++ Dima.Api/Endpoints/Orders/GetAllProductsEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class GetAllProductsEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/", HandleAsync)
+            .WithName("Products: Get All")
+            .WithSummary("Obtém todos os produtos")
+            .WithDescription("Obtém todos os produtos")
+            .WithOrder(1)
+            .Produces<PagedResponse<List<Order>?>>();
+
+    private static async Task<IResult> HandleAsync(
+        IProductHandler handler,
+        [FromQuery] int pageNumber = Configuration.DefaultPageNumber,
+        [FromQuery] int pageSize = Configuration.DefaultPageSize)
+    {
+        var request = new GetAllProductsRequest
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+
+        var result = await handler.GetAllAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
---
+++ Dima.Api/Endpoints/Orders/GetProductBySlugEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class GetProductBySlugEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/{slug}", HandleAsync)
+            .WithName("Product: Get by Slug")
+            .WithSummary("Obtém um produto pelo slug")
+            .WithDescription("Obtém um produto pelo slug")
+            .WithOrder(2)
+            .Produces<Response<Product?>>();
+
+    private static async Task<IResult> HandleAsync(IProductHandler handler, [FromRoute] string slug)
+    {
+        var request = new GetProductBySlugRequest
+        {
+            Slug = slug
+        };
+
+        var result = await handler.GetBySlugAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
