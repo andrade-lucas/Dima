64f9f7ab07475fc7ecd51654a0636b6e9d851ecd
Lucas Andrade <lucasilva108@gmail.com>
2024-11-07 22:03:21 -0300

    chore: list products code behind

---
+++ Dima.Web/Pages/Products/List.razor.cs
@@ -0,0 +1,55 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Products;
+
+public partial class ListProducsPage : ComponentBase
+{
+    #region Properties
+
+    public bool IsBusy { get; set; } = false;
+    public List<Product> Products { get; set; } = [];
+
+    #endregion
+
+    #region Services
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    [Inject]
+    public IProductHandler Handler { get; set; } = null!;
+
+    #endregion
+
+    #region Overrides
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsBusy = true;
+        try
+        {
+            var request = new GetAllProductsRequest();
+            var result = await Handler.GetAllAsync(request);
+
+            if (result is { IsSuccess: true, Data: not null })
+                Products = result.Data;
+            else
+                Snackbar.Add("Erro ao obter produtos", Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    #endregion
+}
