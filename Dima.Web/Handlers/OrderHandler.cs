118eabce2b42ce537b797bac228d3c091f80a0c3
Lucas Andrade <lucasilva108@gmail.com>
2024-11-07 22:02:33 -0300

    feat: implement OrderHandler on web app

--- Dima.Web/Handlers/OrderHandler.cs
+++ Dima.Web/Handlers/OrderHandler.cs
@@ -2,38 +2,55 @@
 using Dima.Core.Models;
 using Dima.Core.Requests.Orders;
 using Dima.Core.Responses;
+using System.Net.Http.Json;
 
 namespace Dima.Web.Handlers;
 
-public class OrderHandler : IOrderHandler
+public class OrderHandler(IHttpClientFactory httpClientFactory) : IOrderHandler
 {
-    public Task<Response<Order?>> CancelAsync(CancelOrderRequest request)
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
+    public async Task<Response<Order?>> CancelAsync(CancelOrderRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PostAsJsonAsync($"/v1/orders/{request.Id}/cancel", request);
+
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>()
+            ?? new Response<Order?>(null, 400, "Erro ao cancelar pedido");
     }
 
-    public Task<Response<Order?>> CreateAsync(CreateOrderRequest request)
+    public async Task<Response<Order?>> CreateAsync(CreateOrderRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PostAsJsonAsync("/v1/orders", request);
+
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>()
+            ?? new Response<Order?>(null, 400, "Erro ao criar pedido");
     }
 
-    public Task<PagedResponse<List<Order>?>> GetAllAsync(GetAllOrdersRequest request)
+    public async Task<PagedResponse<List<Order>?>> GetAllAsync(GetAllOrdersRequest request)
     {
-        throw new NotImplementedException();
+        return await _client.GetFromJsonAsync<PagedResponse<List<Order>?>>("/v1/orders")
+            ?? new PagedResponse<List<Order>?>(null, 400, "Erro ao obter pedidos");
     }
 
-    public Task<Response<Order?>> GetByNumberAsync(GetOrderByNumberRequest request)
+    public async Task<Response<Order?>> GetByNumberAsync(GetOrderByNumberRequest request)
     {
-        throw new NotImplementedException();
+        return await _client.GetFromJsonAsync<Response<Order?>>($"v1/orders/{request.Number}")
+            ?? new Response<Order?>(null, 400, "Erro ao obter pedido");
     }
 
-    public Task<Response<Order?>> PayAsync(PayOrderRequest request)
+    public async Task<Response<Order?>> PayAsync(PayOrderRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PostAsJsonAsync($"/v1/orders/{request.Id}/pay", request);
+
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>()
+            ?? new Response<Order?>(null, 400, "Erro ao pagar pedido");
     }
 
-    public Task<Response<Order?>> RefundAsync(RefundOrderRequest request)
+    public async Task<Response<Order?>> RefundAsync(RefundOrderRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PostAsJsonAsync($"/v1/orders/{request.Id}/refund", request);
+
+        return await result.Content.ReadFromJsonAsync<Response<Order?>>()
+            ?? new Response<Order?>(null, 400, "Erro ao estornar pedido");
     }
 }
